f0b6082179ebf54a1e97e48281ae70f430553430
marcvaldez <marc_fv@yahoo.com>
2019-10-01 19:25:57 -0300

    Initial commit

---
+++ Peer2PeerChat/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Peer2PeerChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtIpAddress = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tmrListenTimer = new System.Windows.Forms.Timer(this.components);
+            this.tmrReceiveMessageTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(11, 394);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(241, 20);
+            this.txtMessage.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(258, 392);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(11, 67);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtOutput.Size = new System.Drawing.Size(322, 307);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.Location = new System.Drawing.Point(11, 31);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(135, 20);
+            this.txtIpAddress.TabIndex = 1;
+            this.txtIpAddress.Text = "127.0.0.1";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(258, 29);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "IP address to connect to";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(152, 31);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "44444";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Port";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 429);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtIpAddress);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "Form1";
+            this.Text = "Peer2PeerChat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtIpAddress;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer tmrListenTimer;
+        private System.Windows.Forms.Timer tmrReceiveMessageTimer;
+    }
+}
+
---
+++ Peer2PeerChat/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Peer2PeerChat
+{
+    public partial class Form1 : Form
+    {
+        // We declare two sockets: one for listening for incoming connection requests 
+        // and another for handling the actual communication (i.e. sending and receiving messages)
+        Socket connectionSocket = null;
+        Socket messageSocket = null;
+
+        // We want to stop listening for incoming connections when we are already connected to someone or if we initiate the connection ourself
+        bool stopListening = false;
+
+        // These two lines of code will get the local ip address and port number from the config file and store them into global variables
+        string ipAddress = ConfigurationManager.AppSettings["ipaddress"];
+        int port = int.Parse(ConfigurationManager.AppSettings["port"]);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // when the form loads, create a socket and wait for incoming connections
+            IPAddress localAddress = IPAddress.Parse(ipAddress);
+            IPEndPoint endpoint = new IPEndPoint(localAddress, port);
+            connectionSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            connectionSocket.Bind(endpoint);
+            connectionSocket.Listen(1);
+
+            // start the timer for listening to connection requests
+            tmrListenTimer.Interval = 1000;
+            tmrListenTimer.Start();
+            tmrListenTimer.Tick += tmrListenTimer_Tick;
+
+            // start the timer for receiving messages
+            tmrReceiveMessageTimer.Interval = 200;
+            tmrReceiveMessageTimer.Start();
+            tmrReceiveMessageTimer.Tick += TmrReceiveMessageTimer_Tick;
+
+            // here we display the ip address and the port number on the top of the form
+            Text = "P2P Chat - " + ipAddress + ":" + port;
+        }
+
+        // This method is executed every second. Running this code will accept any pending connection requests to our socket.
+        private void tmrListenTimer_Tick(object sender, EventArgs e)
+        {
+            // we only want to listen to connection requests if we aren't connected yet, hence this flag
+            if (!stopListening)
+            {
+                Output("Waiting for connection...");
+
+                // the code inside the curly braces below will run after the socket accepts the connection.
+                connectionSocket.BeginAccept((ar) =>
+                {
+                    if (messageSocket == null)
+                    {
+                        messageSocket = connectionSocket.EndAccept(ar);
+                        Output("Connected...");
+
+                        stopListening = true;
+                    }
+                }, connectionSocket);
+            }
+        }
+
+        private void TmrReceiveMessageTimer_Tick(object sender, EventArgs e)
+        {
+            if (messageSocket != null && messageSocket.Connected)
+            {
+                // receive incoming message
+                byte[] buffer = new byte[1000];
+
+                messageSocket.BeginReceive(buffer, 0, 1000, SocketFlags.None, new AsyncCallback(ReceiveCallback), buffer);
+            }
+        }
+
+        private void ReceiveCallback(IAsyncResult asyncResult)
+        {
+            byte[] buffer = (byte[])asyncResult.AsyncState;
+            int bytesRead = messageSocket.EndReceive(asyncResult);
+            if (bytesRead > 0)
+            {
+                string msg = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Output(msg);
+
+                // Get the rest of the data.  
+                buffer = new byte[1000];
+                messageSocket.BeginReceive(buffer, 0, 1000, 0, new AsyncCallback(ReceiveCallback), buffer);
+            }
+        }
+
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            // stop listening
+            stopListening = true;
+            connectionSocket.Close();
+
+            // create a socket for sending messages
+            IPAddress localAddress = IPAddress.Parse(ipAddress);
+            IPEndPoint endpoint = new IPEndPoint(localAddress, port);
+            messageSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            messageSocket.Bind(endpoint);
+
+            // connect to your buddy's device
+            IPAddress buddyAddress = IPAddress.Parse(txtIpAddress.Text);
+            int buddyPort = int.Parse(txtPort.Text);
+            messageSocket.Connect(buddyAddress, buddyPort);
+
+            Output("Connected...");
+
+            // disable the connect button
+            btnConnect.Enabled = false;
+
+            // set the focus on the chat input textbox
+            txtMessage.Focus();
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            string message = txtMessage.Text;
+            Output(message);
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            messageSocket.Send(buffer);
+
+            // clear the chat input
+            txtMessage.Clear();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        delegate void SetTextCallback(string text);
+
+        private void Output(string text)
+        {
+            // What all these complicated code does is just to be able to add text to txtOutput 
+            // from different threads. We actually use threads when executing code through timers
+            if (this.txtOutput.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(Output);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.txtOutput.AppendText(text + Environment.NewLine);
+            }
+        }
+    }
+}
